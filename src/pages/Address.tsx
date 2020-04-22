5c27ec9cf441501d09641a646aab72ca664d5983
홍성준 <sjhong@jjsoft.kr>
2020-04-22 22:14:28 +0900

    [UX/UI]: 리스트 UI 적용

--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -32,6 +32,13 @@ class Address extends React.PureComponent<TypeProps> {
           <button onClick={this.onAddressAdd}>+ 추가</button>
         </div>
         <AddressList />
+        <button className="more-btn">+ 더 보기</button>
+        <div className="notice">
+          <h4>배송지를 삭제하면 예약된 후원의 배송지 정보도 삭제되나요?</h4>
+          <p>
+            현재 후원하신 프로젝트에 등록된 배송지가 삭제되거나 변경되지 않습니다. 이를 변경하시려면 후원현황에서 변경해주세요. 내 후원현황 바로가기
+          </p>
+        </div>
       </div>
     )
   }
