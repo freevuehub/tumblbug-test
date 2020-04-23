bc9de273a92390294304c910748469023c2f908a
freevuehub <hackish@hanmail.net>
2020-04-23 23:33:31 +0900

    [refactor]: 주소 공지사항 분리 [refactor]: 불필요 리소스 제거

--- src/index.tsx
+++ src/index.tsx
@@ -12,12 +12,17 @@ const rootElement: HTMLElement | null = document.getElementById('root')
 // @ts-ignore
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(reducers, devTools)
+const System = React.createContext({
+  test: false,
+})
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <System.Provider value={{ test: false }}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </System.Provider>
   </Provider>,
   rootElement,
 )
--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchAddress } from '../reducers/Address'
 import { loadAddress } from '../axios'
-import { AddressStoreState, AddressItem } from '../types'
+import { AddressStoreState } from '../types'
 import { AddressList, AddForm } from '../containers'
-import { Maybe, AddHeader } from '../components'
+import { Maybe, AddHeader, AddressNotice } from '../components'
 
 interface TypeAddress {
   Address: AddressStoreState
@@ -44,14 +44,7 @@ class Address extends React.PureComponent<TypeProps, TypeState> {
             <button onClick={(event): void => this.onAddressAddPop(event, true)}>+ 추가</button>
           </div>
           <AddressList />
-          <div className="notice">
-            <h4>배송지를 삭제하면 예약된 후원의 배송지 정보도 삭제되나요?</h4>
-            <p>
-              현재 후원하신 프로젝트에 등록된 배송지가 삭제되거나 변경되지 않습니다. 이를
-              변경하시려면 후원현황에서 변경해주세요.
-              <a href="#">내 후원현황 바로가기</a>
-            </p>
-          </div>
+          <AddressNotice />
         </div>
         <Maybe if={this.state.popView}>
           <div className="add-address-pop">
