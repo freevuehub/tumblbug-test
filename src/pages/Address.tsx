ed505fabdba718779ecca570f025ffaa77e80c7a
freevuehub <hackish@hanmail.net>
2020-04-23 14:39:42 +0900

    [new]: 주소 등록하는 팝업 영역 추가

--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -4,6 +4,7 @@ import { fetchAddress } from '../reducers/Address'
 import { loadAddress } from '../axios'
 import { AddressStoreState, AddressItem } from '../types'
 import { AddressList } from '../containers'
+import { Maybe } from '../components'
 
 interface TypeAddress {
   Address: AddressStoreState
@@ -12,7 +13,15 @@ interface TypeProps extends TypeAddress {
   fetchAddress: Function
 }
 
-class Address extends React.PureComponent<TypeProps> {
+interface TypeState {
+  popView: boolean
+}
+
+class Address extends React.PureComponent<TypeProps, TypeState> {
+  state: TypeState = {
+    popView: false,
+  }
+
   async componentDidMount() {
     const { fetchAddress } = this.props
     const { data }: any = await loadAddress()
@@ -24,26 +33,36 @@ class Address extends React.PureComponent<TypeProps> {
     fetchAddress({ ...data, addresses: [defaultItem, ...data.addresses] })
   }
 
-  onAddressAdd() {
+  onAddressAdd(event: any) {
+    event.preventDefault()
+
+    this.setState({
+      popView: true,
+    })
     console.log('aa', this)
   }
 
   render(): React.ReactElement {
     return (
-      <div className="content">
-        <div className="header">
-          <h2>등록된 배송지</h2>
-          <button onClick={this.onAddressAdd}>+ 추가</button>
+      <>
+        <div className="content">
+          <div className="header">
+            <h2>등록된 배송지</h2>
+            <button onClick={(event) => this.onAddressAdd(event)}>+ 추가</button>
+          </div>
+          <AddressList />
+          <div className="notice">
+            <h4>배송지를 삭제하면 예약된 후원의 배송지 정보도 삭제되나요?</h4>
+            <p>
+              현재 후원하신 프로젝트에 등록된 배송지가 삭제되거나 변경되지 않습니다. 이를 변경하시려면 후원현황에서 변경해주세요.
+              <a href="#">내 후원현황 바로가기</a>
+            </p>
+          </div>
         </div>
-        <AddressList />
-        <div className="notice">
-          <h4>배송지를 삭제하면 예약된 후원의 배송지 정보도 삭제되나요?</h4>
-          <p>
-            현재 후원하신 프로젝트에 등록된 배송지가 삭제되거나 변경되지 않습니다. 이를 변경하시려면 후원현황에서 변경해주세요.
-            <a href="#">내 후원현황 바로가기</a>
-          </p>
-        </div>
-      </div>
+        <Maybe if={this.state.popView}>
+          <div className="add-address-pop">add-address-pop</div>
+        </Maybe>
+      </>
     )
   }
 }
